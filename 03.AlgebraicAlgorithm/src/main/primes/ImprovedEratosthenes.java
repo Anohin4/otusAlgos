cd179acadd18bcf7fedc6eed575662998dcff31f
Anokhin Evgeniy <enanohin@mts.ru>
2022-05-16 10:58:11 +0300

    выкинул генережку матрицы

--- 03.AlgebraicAlgorithm/src/main/primes/ImprovedEratosthenes.java
+++ 03.AlgebraicAlgorithm/src/main/primes/ImprovedEratosthenes.java
@@ -5,36 +5,33 @@ public class ImprovedEratosthenes {
     public static void main(String[] args) {
         System.out.println(countPrimeNumbers(10000));
     }
-    static int counter;
 
     public static long countPrimeNumbers(int n) {
-        int p = 2;
-        counter = 1;
+        int counter = 0;
         int[] arrayOfAllPrimes = new int[n/2];
-        arrayOfAllPrimes[0] = 2;
         int[] arrayOfAllNumbers = new int[n];
-        //генерим массив
-        for (int j = 3; j < n; j++) {
-            arrayOfAllNumbers[j] = j;
-        }
 
-        for (; p< n; p++) {
+        for (int p = 2; p< n; p++) {
+            //число не изменилось за прошлые итерации - значит простое
+            if (arrayOfAllNumbers[p] == 0) {
+                //вычеркиваем квадрат числа
+                if(p*p < n) {
+                    arrayOfAllNumbers[p*p] = 1;
+                }
+                arrayOfAllPrimes[counter] = p;
+                counter++;
+            }
+
+            //проставляем 1 в те числа, которые точно не простые
             for (int i = 0; i < counter; i++) {
                 int j = p * arrayOfAllPrimes[i];
                 //защита от arrayOutOfBound
                 if(j >= n ) {
                     continue;
                 }
-                arrayOfAllNumbers[j] = 0;
+                arrayOfAllNumbers[j] = 1;
             }
-            //число не изменилось за прошлые итерации - значит простое
-            if (arrayOfAllNumbers[p] == p) {
-                if(p*p < n) {
-                    arrayOfAllNumbers[p*p] = 0;
-                }
-                arrayOfAllPrimes[counter] = p;
-                counter++;
-            }
+
         }
         return counter;
     }
