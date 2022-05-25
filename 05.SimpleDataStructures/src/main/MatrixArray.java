cfcd3133ce14e04663b9829d6e84d6e02d157d7a
Anokhin Evgeniy <enanohin@mts.ru>
2022-05-25 17:25:20 +0300

    style fix

--- 05.SimpleDataStructures/src/main/MatrixArray.java
+++ 05.SimpleDataStructures/src/main/MatrixArray.java
@@ -4,12 +4,12 @@ import static java.util.Objects.nonNull;
 
 public class MatrixArray<T> implements MyDataStructure<T> {
 
-    private MyDataStructure<MyDataStructure<T>> array;
+    private MyDataStructure<MyDataStructure<T>> arrayOfArrays;
     int currentSize;
     int factor;
 
     public MatrixArray(int factor) {
-        array = new SingleArray<>();
+        arrayOfArrays = new SingleArray<>();
         this.factor = factor;
         this.currentSize = 0;
     }
@@ -21,26 +21,26 @@ public class MatrixArray<T> implements MyDataStructure<T> {
 
     @Override
     public void add(T item) {
-        if (currentSize == factor * array.size()) {
-            array.add(new VectorArray<>(factor));
+        if (currentSize == factor * arrayOfArrays.size()) {
+            arrayOfArrays.add(new VectorArray<>(factor));
         }
-        array.get(currentSize / factor).add(item);
+        arrayOfArrays.get(currentSize / factor).add(item);
         currentSize++;
     }
 
     @Override
     public T get(int position) {
-        return array.get(position / factor).get(position % factor);
+        return arrayOfArrays.get(position / factor).get(position % factor);
     }
 
     @Override
     public void add(T item, int position) {
-        if (currentSize == factor * array.size() ) {
-            array.add(new VectorArray<>(factor));
+        if (currentSize == factor * arrayOfArrays.size() ) {
+            arrayOfArrays.add(new VectorArray<>(factor));
         }
 
         int numberOfArray = position /factor;
-        MyDataStructure<T> workingArray = array.get(numberOfArray);
+        MyDataStructure<T> workingArray = arrayOfArrays.get(numberOfArray);
         if(workingArray.size() != factor) {
             //если массив не полный - он последний, просто добавляем
             workingArray.add(item, position % factor);
@@ -52,8 +52,8 @@ public class MatrixArray<T> implements MyDataStructure<T> {
             numberOfArray++;
 
             //сдвигаем все значения в остальных массивах
-            for (int i = numberOfArray; i < array.size() - 1; i++) {
-                workingArray = array.get(i);
+            for (int i = numberOfArray; i < arrayOfArrays.size() - 1; i++) {
+                workingArray = arrayOfArrays.get(i);
                 T removed = workingArray.remove(workingArray.size() - 1);
                 workingArray.add(tempFromPreviousArray, 0);
                 numberOfArray++;
@@ -61,7 +61,7 @@ public class MatrixArray<T> implements MyDataStructure<T> {
             }
 
             //в последний массив добавляем значение в начало
-            array.get(array.size() - 1)
+            arrayOfArrays.get(arrayOfArrays.size() - 1)
                     .add(tempFromPreviousArray, 0);
         }
         currentSize++;
@@ -70,28 +70,28 @@ public class MatrixArray<T> implements MyDataStructure<T> {
     @Override
     public T remove(int position) {
         int numberOfArray = position /factor;
-        MyDataStructure<T> workingArray = array.get(numberOfArray);
+        MyDataStructure<T> workingArray = arrayOfArrays.get(numberOfArray);
         T removedElement = workingArray.remove(position%factor);
         currentSize --;
         //дальнейшие действия актуальны только если данный массив не последний
-        if(numberOfArray < array.size() - 1) {
+        if(numberOfArray < arrayOfArrays.size() - 1) {
             //добавляем на последнее место массива первый элемент следующего массива
-            workingArray.add(array.get(numberOfArray + 1).get(0));
+            workingArray.add(arrayOfArrays.get(numberOfArray + 1).get(0));
             numberOfArray++;
             //делаем сдвиг во всех остальных массивах кроме последнего
-            for (int i = numberOfArray; i < array.size() - 1; i++) {
-                workingArray = array.get(i);
+            for (int i = numberOfArray; i < arrayOfArrays.size() - 1; i++) {
+                workingArray = arrayOfArrays.get(i);
                 workingArray.remove(0);
-                workingArray.add(array.get(i + 1).get(0));
+                workingArray.add(arrayOfArrays.get(i + 1).get(0));
             }
             //для последнего массива просто удаляем первый элемент
-            array.get(array.size() - 1).remove(0);
+            arrayOfArrays.get(arrayOfArrays.size() - 1).remove(0);
         }
 
         //проверка, что последний элемент не пустой
         //пустой - удаляем
-        if (!nonNull(array.get(array.size() - 1).get(0))) {
-            array.remove(array.size() - 1);
+        if (!nonNull(arrayOfArrays.get(arrayOfArrays.size() - 1).get(0))) {
+            arrayOfArrays.remove(arrayOfArrays.size() - 1);
         }
         return removedElement;
     }
@@ -99,8 +99,8 @@ public class MatrixArray<T> implements MyDataStructure<T> {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < array.size(); i++) {
-            sb.append(array.get(i));
+        for (int i = 0; i < arrayOfArrays.size(); i++) {
+            sb.append(arrayOfArrays.get(i));
         }
         return sb + "\n current size " + currentSize;
     }
